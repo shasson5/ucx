65fa558b36ee9378edf7c5fa2dc62d2a9ee3576a
Shachar Hasson <shasson5@gmail.com>
2023-06-06 19:22:01 +0300

    UCS/DATA: linked list is internal

--- src/ucs/datastruct/lru.c
+++ src/ucs/datastruct/lru.c
@@ -9,19 +9,25 @@
 #  include "config.h"
 #endif
 
-
 #include "lru.h"
 #include "ucs/debug/memtrack_int.h"
 
 
-__KHASH_TYPE(lru_hash, uint64_t, ucs_list_link_t);
+typedef struct {
+    void           *key;
+    ucs_list_link_t list;
+} ucs_lru_element_t;
 
 
-__KHASH_IMPL(lru_hash, kh_inline, uint64_t, ucs_list_link_t, 0,
+__KHASH_TYPE(lru_hash, uint64_t, ucs_lru_element_t);
+
+
+__KHASH_IMPL(lru_hash, kh_inline, uint64_t, ucs_lru_element_t, 1,
              kh_int64_hash_func, kh_int64_hash_equal);
 
 
 typedef khash_t(lru_hash) ucs_lru_hash_t;
+
 
 typedef struct ucs_lru {
     ucs_lru_hash_t  hash;
@@ -74,26 +80,31 @@ static void ucs_lru_pop(ucs_lru_h lru)
     ucs_list_del(lru->list.prev);
 }
 
-static void ucs_lru_push(ucs_lru_h lru, ucs_list_link_t *elem)
+static void ucs_lru_push(ucs_lru_h lru, ucs_lru_element_t *elem)
 {
-    ucs_list_add_head(&lru->list, elem);
+    ucs_list_add_head(&lru->list, &elem->list);
 }
 
 //todo: optimize fast path with ucs_likely.
 
-ucs_status_t ucs_lru_touch(ucs_lru_h lru, ucs_list_link_t *elem)
+ucs_status_t ucs_lru_touch(ucs_lru_h lru, void *key)
 {
     khint_t iter;
     int ret;
+    ucs_lru_element_t *elem;
 
-    iter = kh_put(lru_hash, &lru->hash, (uint64_t)elem, &ret);
+    iter = kh_put(lru_hash, &lru->hash, (uint64_t)key, &ret);
     if (ret == UCS_KH_PUT_FAILED) {
         //todo: replace error code.
+        //todo: maybe should ignore this error because it can never happen.
         return UCS_ERR_NO_RESOURCE;
     }
 
+    elem      = &kh_val(&lru->hash, iter);
+    elem->key = key;
+
     if (ret == UCS_KH_PUT_KEY_PRESENT) {
-        ucs_list_del(elem);
+        ucs_list_del(&elem->list);
     } else if (lru->size == lru->capacity) {
         ucs_lru_pop(lru);
         kh_del(lru_hash, &lru->hash, iter);
@@ -105,12 +116,12 @@ ucs_status_t ucs_lru_touch(ucs_lru_h lru, ucs_list_link_t *elem)
     return UCS_OK;
 }
 
-void ucs_lru_get(const ucs_lru_h lru, ucs_list_link_t **elements, size_t *size_p)
+void ucs_lru_get(const ucs_lru_h lru, void **elements, size_t *size_p)
 {
-    ucs_list_link_t *elem;
+    ucs_list_link_t *link;
 
-    for (elem = lru->list.next; elem != &lru->list; elem = elem->next) {
-        *elements = elem;
+    for (link = lru->list.next; link != &lru->list; link = link->next) {
+        *elements = ucs_container_of(link, ucs_lru_element_t, list)->key;
         elements ++;
     }
 
--- src/ucs/datastruct/lru.h
+++ src/ucs/datastruct/lru.h
@@ -12,7 +12,7 @@ typedef struct ucs_lru *ucs_lru_h;
 
 ucs_lru_h ucs_lru_init(size_t capacity);
 void ucs_lru_destroy(ucs_lru_h lru);
-ucs_status_t ucs_lru_touch(ucs_lru_h lru, ucs_list_link_t *elem);
-void ucs_lru_get(const ucs_lru_h lru, ucs_list_link_t **elements, size_t *size);
+ucs_status_t ucs_lru_touch(ucs_lru_h lru, void *key);
+void ucs_lru_get(const ucs_lru_h lru, void **elements, size_t *size);
 
 #endif
